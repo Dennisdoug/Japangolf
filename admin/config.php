113fdb60b93731710302cd0906ee2bfc629f05cc
Dung Tran <ddt1328@outlook.com.vn>
2018-09-03 15:25:50 +0700

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/japangolf/admin/');
+define('HTTP_CATALOG', 'http://localhost/japangolf/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/japangolf/admin/');
+define('HTTPS_CATALOG', 'http://localhost/japangolf/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp64/www/Japangolf/admin/');
+define('DIR_SYSTEM', 'C:/wamp64/www/Japangolf/system/');
+define('DIR_LANGUAGE', 'C:/wamp64/www/Japangolf/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp64/www/Japangolf/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp64/www/Japangolf/system/config/');
+define('DIR_IMAGE', 'C:/wamp64/www/Japangolf/image/');
+define('DIR_CACHE', 'C:/wamp64/www/Japangolf/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp64/www/Japangolf/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp64/www/Japangolf/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp64/www/Japangolf/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp64/www/Japangolf/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp64/www/Japangolf/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Inventory');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/japangolf/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/japangolf/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp64/www/Japangolf/catalog/');
+define('DIR_SYSTEM', 'C:/wamp64/www/Japangolf/system/');
+define('DIR_LANGUAGE', 'C:/wamp64/www/Japangolf/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp64/www/Japangolf/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp64/www/Japangolf/system/config/');
+define('DIR_IMAGE', 'C:/wamp64/www/Japangolf/image/');
+define('DIR_CACHE', 'C:/wamp64/www/Japangolf/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp64/www/Japangolf/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp64/www/Japangolf/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp64/www/Japangolf/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp64/www/Japangolf/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Inventory');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
